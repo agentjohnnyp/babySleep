f3552f187b2c66a5c8873171aa7b070ad6467251
AgentJohnnyP <john.prinz@gmail.com>
2013-07-09 13:03:30 -0400

    Added structure for line graph.

--- php/graph.php
+++ php/graph.php
@@ -1,4 +1,34 @@
-<?php
+<?php 
 
+// set content type so that image will be rendered in the browser ... remember in jsp examples set text/plain 
+header ("Content-type: image/png");
+
+//create new 300 x 300 image
+$myImage = ImageCreate(300,200);
+
+// the first color allocated is the background color
+$white = ImageColorAllocate($myImage, 0xff, 0xff, 0xff);
+$red = ImageColorAllocate($myImage, 0xff, 0, 0);
+$green = ImageColorAllocate($myImage, 0, 0xff, 0);
+
+
+
+// Draw a green rectangle around the edge of the screen
+ImageRectangle($myImage, 0, 0, 299, 199, $green);
+
+// Start drawing line on graph
+ImageLine($myImage, 10, 40, 50, 10, $green);
+ImageLine($myImage, 50, 10, 90, 60, $green);
+ImageLine($myImage, 90, 60, 130, 30, $green);
+ImageLine($myImage, 130, 30, 170, 70, $green);
+ImageLine($myImage, 170, 70, 210, 40, $green);
+ImageLine($myImage, 210, 40, 250, 50, $green);
+
+
+// send the bytes of the image
+ImagePng($myImage);
+
+// free the memory for the image
+ImageDestroy($myImage);
 
 ?>
